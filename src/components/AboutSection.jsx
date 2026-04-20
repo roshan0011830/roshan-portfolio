14ef96673e100a938b3ee7da0a055d67aa184dac
Roshan pal <pal9122088@gmail.com>
2026-04-20 12:32:29 +0530

    download CV working fine

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -38,7 +38,8 @@ function AboutSection() {
               </a>
 
               <a
-                href="#contact"
+                href="/myResume.pdf"
+                download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 {" "}
